8dcb733f39a2cf55fedeecb4d69cd9b8b9ca9d86
Gyrateplace <gyrateplace@gmail.com>
2025-08-27 17:16:02 -0500

    Add everything

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,43 @@
-console.log("JavaScript working!");
+const taskInput = document.getElementById('taskInput');
+const addBtn = document.getElementById('addBtn');
+const taskList = document.getElementById('taskList');
+
+//Create Task
+addBtn.addEventListener('click', addTask);
+taskInput.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') addTask();
+});
+
+function addTask() {
+  const taskText = taskInput.value.trim();
+  if (taskText === "") return;
+
+  const li = document.createElement('li');
+
+  //Check task
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.addEventListener('change', () => {
+    li.classList.toggle('completed');
+  });
+
+  const span = document.createElement('span');
+  span.textContent = taskText;
+  span.classList.add('task-text');
+
+  //Delete Task
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent = 'Eliminar';
+  deleteBtn.classList.add('delete-btn');
+  deleteBtn.addEventListener('click', () => {
+    taskList.removeChild(li);
+  });
+
+  li.appendChild(checkbox);
+  li.appendChild(span);
+  li.appendChild(deleteBtn);
+  taskList.appendChild(li);
+
+  taskInput.value = "";
+  taskInput.focus();
+}
